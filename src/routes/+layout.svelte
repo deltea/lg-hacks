0e45c4225e961cd51b2cdd6848ac6958cbccbdf3
deltea <thcheetah777@gmail.com>
2025-03-23 08:37:54 -0700

    🍞 init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "../app.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>Bread</title>
+</svelte:head>
+
+<div class="h-screen">
+  {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<main class="h-full">
+  <h1 class="text-4xl font-bold">Bread</h1>
+</main>
