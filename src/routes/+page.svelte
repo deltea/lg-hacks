e6b5cced6e940db55186309a1b6565259e694020
deltea <thcheetah777@gmail.com>
2025-03-23 17:28:40 -0700

    make home links clickable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,13 +12,11 @@
   <ul class="grid gap-4 grid-cols-3 w-[60rem]">
     {#each data.classes as slice (slice.id)}
       <li class="bg-stone-800 p-4 rounded-md group h-60 hover:cursor-pointer">
-        <a href="/classes/{slice.slug}" class="w-full h-full flex-col">
+        <a href="/classes/{slice.slug}" class="w-full h-full flex-col block">
           <div class="flex items-center justify-between w-full">
             <span class="group-hover:underline">{slice.name}</span>
             <span class="text-stone-600">period {slice.period}</span>
           </div>
-
-
         </a>
       </li>
     {/each}
